aedbaf82ea73df0c271e254d026443b433a6899f
Rafał Cendrowski <01168875@pw.edu.pl>
2024-06-03 21:08:49 +0200

    fix db connectivity

--- quizwiz-backend/src/main/java/pl/edu/pw/quizwiz/config/SecurityConfig.java
+++ quizwiz-backend/src/main/java/pl/edu/pw/quizwiz/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(it -> it.configure(http))
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -31,10 +32,11 @@ public class SecurityConfig {
                                 "/swagger-ui/**"
                         ).permitAll()
                         .anyRequest().authenticated())
-                .authenticationProvider(getAuthenticationProvider());
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
+    @Bean
     public DaoAuthenticationProvider getAuthenticationProvider() {
         final DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
