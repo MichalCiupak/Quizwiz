72009de5370b155d7c18b11c4c80dc00a31bc387
Rafał Cendrowski <01168875@pw.edu.pl>
2024-06-08 17:01:34 +0200

    disable cors

--- quizwiz-backend/src/main/java/pl/edu/pw/quizwiz/config/SecurityConfig.java
+++ quizwiz-backend/src/main/java/pl/edu/pw/quizwiz/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
